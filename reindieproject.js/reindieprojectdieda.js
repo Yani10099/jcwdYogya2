3b7bbd1eca4e2a9906410b4788203275ff6d7093
Yani10099 <freshyanihastuti@gmail.com>
2024-06-28 16:27:55 +0800

    reindieprojectdieda.js

---
+++ reindieproject.js/reindieprojectdieda.js
@@ -0,0 +1,14 @@
+// funtion DieProject { nama : Da Rein 29 fisic : FatShort, Cant Server settle,
+//     notmy type for (i < 10; i = 0; i++);
+
+//     return die die da die rein die dah ) }
+
+function FastDieProject() {
+  const nama = "Dava Rein";
+  const fisic = "FatShort";
+  const cantServerSettle = true;
+
+  for (let i = 0; i < 10; i++) {}
+
+  return "die die da die rein die dah";
+}
